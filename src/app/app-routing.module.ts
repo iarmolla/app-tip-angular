6969c20bd28b7fef1d7aadd53a473fba7509f31a
iarmolla <ivan.armolla@gmail.com>
2023-04-13 19:23:51 -0300

    add app routes for login signup home and 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from './shared/guards/auth.guard';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignupComponent},
-  { path: 'register', component: RegisterComponent},
-  { path: '', component: HomeComponent}
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '**', component: PagenotfoundComponent}
 
 ];
 
