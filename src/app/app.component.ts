7337c92872961ade2da297e05c4d5f3c427959c5
iarmolla <ivan.armolla@gmail.com>
2023-04-15 02:09:47 -0300

    add validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TipService } from './services/tip.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -6,10 +8,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'delivery';
+export class AppComponent implements OnInit {
+  title = 'Tippit';
+  isLoggedIn: boolean = false;
   /**
    *
    */
+  constructor(private tipService: TipService, private router: Router) {
+    this.tipService.isAuthenticated$.subscribe()
+  }
+  ngOnInit(): void {
+    if (localStorage.getItem('auth') !== null) {
+      this.isLoggedIn = true
+    } else {
+      this.tipService.isAuthenticated$.subscribe((data) => {
+        this.isLoggedIn = data
+        if (!this.isLoggedIn) {
+          this.router.navigate(['/login']);
+        }
+      })
+    }
+  }
 
 }
