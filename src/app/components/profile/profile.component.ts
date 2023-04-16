c77986fce98ffce330221f64075b749dfd609b9e
iarmolla <ivan.armolla@gmail.com>
2023-04-15 23:38:07 -0300

    adding  form validations

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { TipService } from 'src/app/services/tip.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,10 +11,11 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup
   profilePicture: any
-  constructor(public dialogRef: MatDialogRef<ProfileComponent>) {
+  submitted = false;
+  constructor(public dialogRef: MatDialogRef<ProfileComponent>, private tipService: TipService) {
     this.profileForm = new FormGroup({
-      email: new FormControl('', [Validators.pattern('^[0-9]+$'), Validators.required]),
-      username: new FormControl('', Validators.required),
+      email: new FormControl(window.localStorage.getItem('email'), [Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'), Validators.required]),
+      username: new FormControl(window.localStorage.getItem('username'), [Validators.pattern('^.{4,}$'),Validators.required]),
       password: new FormControl('', [Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/), Validators.required]),
     })
   }
@@ -24,7 +26,6 @@ export class ProfileComponent implements OnInit {
     } else {
       this.profilePicture = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
     }
-
   }
   onFileSelected(event: any) {
     const file = event.target.files[0];
@@ -35,5 +36,11 @@ export class ProfileComponent implements OnInit {
     };
     reader.readAsDataURL(file);
   }
-
+  get f() { return this.profileForm.controls; }
+  onSubmit() {
+    this.submitted = true;
+    if (this.profileForm.valid) {
+      this.tipService.updateUser(window.localStorage.getItem('auth'), this.profileForm.value)
+    }
+  }
 }
