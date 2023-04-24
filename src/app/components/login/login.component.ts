afb6a78ba2ce40af3314b27b8a4f403e32b7e7e9
iarmolla <ivan.armolla@gmail.com>
2023-04-24 11:55:37 -0300

    changing styles to form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 import { TipService } from 'src/app/services/tip.service';
 
 @Component({
@@ -12,7 +14,7 @@ export class LoginComponent {
   submitted = false;
   error: string = ''
   message: string = '';
-  constructor(private tipServices: TipService) {
+  constructor(private tipServices: TipService, private authService: AuthService, private router: Router) {
     this.registerForm = new FormGroup({
       email: new FormControl('',  [Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),Validators.required]),
       password: new FormControl('', [Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/), Validators.required]),
@@ -23,6 +25,20 @@ export class LoginComponent {
     this.submitted = true;
     if(this.registerForm.valid) {
       this.message = await this.tipServices.signIn(this.registerForm.value)
+      this.authService.login(this.registerForm.value).then((res) => {
+        this.message = ''
+        this.router.navigate(['/'])
+      }).catch((error) => {
+        this.message = 'Ocurrio un error al iniciar sesión'
+      })
     }
+  }
+  loginWithGoogle() {
+    this.authService.loginWithGoogle().then((res) => {
+      this.message = ''
+      this.router.navigate(['/'])
+    }).catch((error) => {
+      this.message = 'Ocurrio un error al iniciar sesión con Google'
+    })
   }
 }
