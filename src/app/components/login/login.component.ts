82588e003b6a62ecef5018c8fa04a5475c26687c
iarmolla <ivan.armolla@gmail.com>
2023-06-09 16:14:25 -0300

    add lowercase to email

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -16,16 +16,17 @@ export class LoginComponent {
   message: string = '';
   constructor(private tipServices: TipService, private authService: AuthService, private router: Router) {
     this.registerForm = new FormGroup({
-      email: new FormControl('',  [Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),Validators.required]),
+      email: new FormControl('', [Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/), Validators.required]),
       password: new FormControl('', [Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/), Validators.required]),
     })
   }
   get f() { return this.registerForm.controls; }
   async onSubmit(): Promise<void> {
     this.submitted = true;
-    if(this.registerForm.valid) {
-      this.message = await this.tipServices.signIn(this.registerForm.value)
-      this.authService.login(this.registerForm.value).then((res) => {
+    if (this.registerForm.valid) {
+      this.f.email.setValue(this.f.email.value.toLowerCase())
+      this.authService.login(this.registerForm.value).then(async (res) => {
+        this.message = await this.tipServices.signIn(this.registerForm.value)
         this.message = ''
         this.router.navigate(['/'])
       }).catch((error) => {
