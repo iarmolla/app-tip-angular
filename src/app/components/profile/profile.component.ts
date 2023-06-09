0d2cdb4a117444036bc3159a7286b8b9d97915f4
iarmolla <ivan.armolla@gmail.com>
2023-06-09 17:33:42 -0300

    add error message

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { getIdToken } from '@angular/fire/auth';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { ImageService } from 'src/app/services/image.service';
 import { TipService } from 'src/app/services/tip.service';
@@ -17,10 +18,10 @@ export class ProfileComponent implements OnInit {
   submitted = false;
   hidePassword: boolean = true;
   hidePasswordTwo: boolean = true;
-  constructor(private authService: AuthService, private imageService: ImageService,public dialogRef: MatDialogRef<ProfileComponent>, private tipService: TipService) {
+  error: string = ''
+  constructor(private authService: AuthService, private router: Router, private imageService: ImageService, public dialogRef: MatDialogRef<ProfileComponent>, private tipService: TipService) {
     this.profileForm = new FormGroup({
       email: new FormControl(window.localStorage.getItem('email'), [Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'), Validators.required]),
-      password: new FormControl('', [Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/), Validators.required]),
       newPassword: new FormControl('', [Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/), Validators.required]),
     })
   }
@@ -28,13 +29,11 @@ export class ProfileComponent implements OnInit {
     const id = window.localStorage.getItem('auth')
     if (id) {
       this.tipService.getUserById(id).subscribe((user: any) => {
-        console.log(user);
-        console.log(user);
         this.profilePicture = user.profileImage || this.imageService.getImage
       })
     }
   }
-  
+
   onFileSelected(event: any) {
     const file = event.target.files[0];
     const reader = new FileReader();
@@ -46,21 +45,17 @@ export class ProfileComponent implements OnInit {
     reader.readAsDataURL(file);
   }
   get f() { return this.profileForm.controls; }
-  onSubmit() {
+  async onSubmit() {
     this.submitted = true;
     if (this.profileForm.valid) {
-      const { email, password, newPassword } = this.profileForm.value
-      const user = {
-        email,
-        password,
+      const { newPassword } = this.profileForm.value
+      const res = await this.authService.updatePassword(newPassword)
+      if (res) {
+        this.error = res
+      } else {
+        window.localStorage.clear()
+        this.router.navigate(['/login']).then(() => window.location.reload())
       }
-      this.authService.updatePassword(user, newPassword).then((res) => {
-        console.log(res)
-        console.log('contraseña actualizada!');
-      }).catch((error) => {
-        console.log(error);
-        console.log('error al actualizar contraseña')
-      })
     }
   }
 
